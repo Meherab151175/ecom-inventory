92abc8d1c88cacb8ef77f3c024a82b923eac174c
meherab151175 <meherabm803@gmail.com>
2025-09-22 00:08:42 +0600

    build error fix

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
-import { AppModule } from 'src/app.module';
+import { AppModule } from './app.module';
 
 const expressApp = express();
 const adapter = new ExpressAdapter(expressApp);
